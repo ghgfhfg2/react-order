5324d360c031fb67209a21721230d41578fc2f06
hyunsoo kim <ghgfhfg2@naver.com>
2021-02-18 15:45:16 +0900

    .

--- src/component/Admin/AdminOrderList.js
+++ src/component/Admin/AdminOrderList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import firebase from "../../firebase";
 import { Radio } from "antd";
+import { commaNumber } from "../CommonFunc";
 
 function AdminOrderList() {
 
@@ -8,6 +9,11 @@ function AdminOrderList() {
   const [SelectDay, setSelectDay] = useState();
   const [LastDay, setLastDay] = useState()
   const [PrevDay, setPrevDay] = useState()
+  const [SumAmount, setSumAmount] = useState()
+  const [SumPrice, setSumPrice] = useState()
+  const [Add1Count, setAdd1Count] = useState()
+  const [Add2Count, setAdd2Count] = useState()
+  const [SumAddAmount, setSumAddAmount] = useState()
   useEffect(() => {
     let mounted = true;
     if (mounted) {
@@ -45,8 +51,54 @@ function AdminOrderList() {
             array = array.filter(el => {
               return el.order_time.includes(SelectDay)
             })
+            let sumA = 0;
+            let sumP = 0;
+            let add1C = 0;
+            let add2C = 0;
+            array.map(el => {
+              sumA += parseInt(el.amount)
+              if(el.add && el.add.includes('버블')) {
+                el.price -= 500;
+                add1C += 1
+              }
+              if(el.add2 && el.add2.includes('샷1')) {
+                el.price -= 500;                
+                add2C += 1
+              }
+              if(el.add2 && el.add2.includes('샷2')) {
+                el.price -= 1000;
+                add2C += 2
+              }              
+            })
+            setSumAmount(sumA)
+            setAdd1Count(add1C)
+            setAdd2Count(add2C)
+            setSumAddAmount(add1C+add2C)  
+            array = array.reduce((a,c)=>{
+              let x = a.find(e=>(e.prod_name===c.prod_name && e.hot===c.hot))
+              if(!x) a.push(Object.assign({},c))
+              else {
+                x.og_price += c.og_price
+                x.amount += c.amount
+              };
+              return a
+            },[])                 
+            array.map(el => {
+              sumP += el.price * el.amount
+              el.price = el.price * el.amount
+            })     
+            setSumPrice(sumP)
+            console.log(array)
+            array.sort((a,b) => {
+              if (a.category > b.category) {
+                return -1;
+              }
+              if (a.category < b.category) {
+                return 1;
+              }
+            })
           }
-          setOrderList(array);
+          setOrderList(array);          
         });
       }
       return function cleanup() {
@@ -66,7 +118,6 @@ function AdminOrderList() {
         setSelectDay(PrevDay)
       }
     }        
-
   return (
     <>
       <h3 className="title">완료내역</h3>
@@ -76,39 +127,107 @@ function AdminOrderList() {
         <Radio.Button value="3">어제</Radio.Button>
       </Radio.Group>
       <span style={{fontSize:"13px",marginLeft:"5px"}}>(영업일 기준)</span>
-      <table className="fl-table" style={{marginTop:"12px"}}>
+      {!SelectDay && 
+        <table className="fl-table" style={{marginTop:"12px"}}>
+          <thead>
+            <tr>
+              <th scope="col">주문자</th>
+              <th scope="col">상품명</th>
+              <th scope="col">수량</th>
+              <th scope="col" colSpan="2">추가</th>
+              <th scope="col">코멘트</th>
+              <th scope="col">주문시간</th>
+              <th scope="col">가격</th>
+            </tr>
+          </thead>
+          <tbody>
+            {OrderList.map((list, index) => (
+              <tr key={index}>
+                <td>{list.order_name}</td>
+                <td>
+                  {list.hot === "hot" && "따뜻한 "}
+                  {list.hot === "ice" && "차가운 "}
+                  {list.prod_name}
+                </td>
+                <td>{list.amount}</td>
+                <td>{list.add}</td>
+                <td>{list.add2}</td>
+                <td>{list.order_etc}</td>
+                <td>{list.order_time}</td>
+                <td>{list.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      }
+      {SelectDay && 
+        <>
+        <div style={{marginTop:"12px"}}>
+        {OrderList[0].order_time}        
+        </div>
+        <div style={{display:"flex"}}>
+        <table className="fl-table" style={{marginTop:"12px",width:"48%"}}>
+          <thead>
+            <tr>
+              <th scope="col">상품명</th>
+              <th scope="col">수량</th>
+              <th scope="col">가격</th>
+            </tr>
+          </thead>
+          <tbody>
+            {OrderList.map((list, index) => (
+              <tr key={index}>
+                <td>
+                  {list.hot === "hot" && "따뜻한 "}
+                  {list.hot === "ice" && "차가운 "}
+                  {list.prod_name}
+                </td>
+                <td>{list.amount}</td>
+                <td>{list.price}</td>
+              </tr>
+            ))}
+            <tr>
+                <td>합계</td>
+                <td>
+                  {SumAmount}
+                </td>
+                <td>{commaNumber(parseInt(SumPrice))}</td>
+            </tr>
+          </tbody>
+        </table>
+        <table className="fl-table" style={{marginTop:"12px",marginLeft:"2%",width:"49%"}}>
         <thead>
-          <tr>
-            <th scope="col">주문자</th>
+          <tr>            
             <th scope="col">상품명</th>
             <th scope="col">수량</th>
-            <th scope="col" colSpan="2">추가</th>
-            <th scope="col">코멘트</th>
-            <th scope="col">주문시간</th>
             <th scope="col">가격</th>
           </tr>
         </thead>
         <tbody>
-          {OrderList.map((list, index) => (
-            <tr key={index}>
-              <td>{list.order_name}</td>
-              <td>
-                {list.hot === "hot" && "따뜻한 "}
-                {list.hot === "ice" && "차가운 "}
-                {list.prod_name}
-              </td>
-              <td>{list.amount}</td>
-              <td>{list.add}</td>
-              <td>{list.add2}</td>
-              <td>{list.order_etc}</td>
-              <td>{list.order_time}</td>
-              <td>{list.price}</td>
+            <tr>
+              <td>버블추가</td>
+              <td>{Add1Count}</td>
+              <td></td>
             </tr>
-          ))}
+            <tr>
+              <td>샷추가</td>
+              <td>{Add2Count}</td>
+              <td></td>
+            </tr>
+          <tr>
+              <td>합계</td>
+              <td>
+                {SumAddAmount}
+              </td>
+              <td>{commaNumber(parseInt(SumAddAmount*500))}</td>
+          </tr>
         </tbody>
       </table>
+      </div>
+      </>
+      }
     </>
-  );
+  );  
 }
 
 export default AdminOrderList;
